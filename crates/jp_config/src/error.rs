929823ed5ecb5757d87d6a479543632cf7a49ee2
Jean Mertz <git@jeanmertz.com>
2025-07-12 00:30:10 +0200

    refactor(cli): Simplify error message formatting
    Error messages printed to stdout already use the `Error::source` method
    to get at the underlying error details. This removes the double printing
    of the first underlying error message.
    
    Signed-off-by: Jean Mertz <git@jeanmertz.com>

--- crates/jp_config/src/error.rs
+++ crates/jp_config/src/error.rs
@@ -4,22 +4,22 @@ pub(crate) type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("Model error: {0}")]
+    #[error("Model error")]
     Model(#[from] jp_model::Error),
 
-    #[error("MCP error: {0}")]
+    #[error("MCP error")]
     Mcp(#[from] jp_mcp::Error),
 
     #[error(transparent)]
     Confique(#[from] confique::Error),
 
-    #[error("IO error: {0}")]
+    #[error("IO error")]
     Io(#[from] std::io::Error),
 
-    #[error("Bool parse error: {0}")]
+    #[error("Bool parse error")]
     ParseBool(#[from] std::str::ParseBoolError),
 
-    #[error("Parse int error: {0}")]
+    #[error("Parse int error")]
     ParseInt(#[from] std::num::ParseIntError),
 
     #[error("Parse float error")]
@@ -48,7 +48,7 @@ pub enum Error {
         error: String,
     },
 
-    #[error("Model slug error: {0}")]
+    #[error("Model slug error")]
     ModelSlug(String),
 
     #[error("Config file not found: {0}")]
@@ -57,22 +57,22 @@ pub enum Error {
     #[error(r#"Invalid or missing file extension: {path}, must be one of "json", "json5", "yaml", "yml" or "toml""#)]
     InvalidFileExtension { path: PathBuf },
 
-    #[error("TOML error: {0}")]
+    #[error("TOML error")]
     Toml(#[from] toml::de::Error),
 
-    #[error("JSON error: {0}")]
+    #[error("JSON error")]
     Json5(#[from] json5::Error),
 
-    #[error("JSON error: {0}")]
+    #[error("JSON error")]
     Json(#[from] serde_json::Error),
 
-    #[error("YAML error: {0}")]
+    #[error("YAML error")]
     Yaml(#[from] serde_yaml::Error),
 
-    #[error("Deserialization error: {0}")]
+    #[error("Deserialization error")]
     Deserialize(#[from] serde::de::value::Error),
 
-    #[error("Failed to serialize XML: {0}")]
+    #[error("Failed to serialize XML")]
     XmlSerialization(#[from] quick_xml::SeError),
 }
 
