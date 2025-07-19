bf82ba5263dcb3b67db05385791a67815b38cf57
Jean Mertz <git@jeanmertz.com>
2025-07-19 02:59:27 +0200

    fixup! chore: Add fs tools for create, delete, and modify operations
    Signed-off-by: Jean Mertz <git@jeanmertz.com>

--- .config/jp/tools/src/fs/utils.rs
+++ .config/jp/tools/src/fs/utils.rs
@@ -13,11 +13,16 @@ pub fn is_file_dirty(root: &Path, file: &Path) -> Result<bool, Error> {
         .current_dir(root)
         .output()?;
 
+    let error = str::from_utf8(&output.stderr).unwrap_or_default();
+    if error.contains("fatal: not a git repository") {
+        return Ok(false);
+    }
+
     if !output.status.success() {
         return Err(format!(
             "Git command failed ({}): {}",
             output.status.code().unwrap_or_default(),
-            String::from_utf8_lossy(&output.stderr)
+            error,
         )
         .into());
     }
