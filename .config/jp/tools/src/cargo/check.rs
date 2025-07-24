21229b32a12a64f691bcf22b4ec4560dde36dced
Jean Mertz <git@jeanmertz.com>
2025-07-24 12:42:48 +0200

    fixup! chore(tools): Add `cargo_check` tool
    Signed-off-by: Jean Mertz <git@jeanmertz.com>

--- .config/jp/tools/src/cargo/check.rs
+++ .config/jp/tools/src/cargo/check.rs
@@ -4,7 +4,7 @@ use crate::{Result, Workspace};
 
 pub(crate) async fn cargo_check(workspace: &Workspace, package: Option<String>) -> Result<String> {
     let package = package.map_or("--workspace".to_owned(), |v| format!("--package={v}"));
-    let result = cmd!("cargo", "check", &package, "--quiet")
+    let result = cmd!("cargo", "check", "--color=never", &package, "--quiet")
         .stdout_capture()
         .stderr_capture()
         .dir(&workspace.path)
@@ -24,7 +24,55 @@ pub(crate) async fn cargo_check(workspace: &Workspace, package: Option<String>) 
     let content = String::from_utf8_lossy(&result.stderr);
     Ok(indoc::formatdoc! {"
         ```
-        {content}
+        {}
         ```
-    "})
+    ", content.trim()})
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_cargo_check() {
+        let dir = tempfile::tempdir().unwrap();
+        let workspace = Workspace {
+            path: dir.path().to_owned(),
+        };
+
+        std::fs::write(dir.path().join("Cargo.toml"), indoc::indoc! {r#"
+            [package]
+            name = "cargo_check"
+        "#})
+        .unwrap();
+
+        std::fs::create_dir_all(dir.path().join("src")).unwrap();
+        std::fs::write(dir.path().join("src/main.rs"), indoc::indoc! {r#"
+            fn main() {
+                std::env::var("FOO");
+            }
+        "#})
+        .unwrap();
+
+        let result = cargo_check(&workspace, None).await.unwrap();
+
+        assert_eq!(result, indoc::indoc! {r#"
+            ```
+            warning: unused `Result` that must be used
+             --> src/main.rs:2:5
+              |
+            2 |     std::env::var("FOO");
+              |     ^^^^^^^^^^^^^^^^^^^^
+              |
+              = note: this `Result` may be an `Err` variant, which should be handled
+              = note: `#[warn(unused_must_use)]` on by default
+            help: use `let _ = ...` to ignore the resulting value
+              |
+            2 |     let _ = std::env::var("FOO");
+              |     +++++++
+            ```
+        "#});
+    }
 }
