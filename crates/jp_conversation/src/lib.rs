1c28e57fa5a0e55f03f1ec26c8e0e1661edabc78
Jean Mertz <git@jeanmertz.com>
2025-11-20 11:54:25 +0100

    fixup! bigly refactor
    Signed-off-by: Jean Mertz <git@jeanmertz.com>

--- crates/jp_conversation/src/lib.rs
+++ crates/jp_conversation/src/lib.rs
@@ -1,11 +1,9 @@
 pub mod conversation;
 pub mod error;
 pub mod event;
-pub mod message;
 pub mod stream;
 pub mod thread;
 
 pub use conversation::{Conversation, ConversationId, ConversationsMetadata};
 pub use error::Error;
-pub use message::MessageId;
 pub use stream::ConversationStream;
